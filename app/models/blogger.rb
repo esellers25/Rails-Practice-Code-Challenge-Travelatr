285b56a9d3d5a06a68c6e11c7a3874bb749bac03
esellers25 <erin.e.sellers@gmail.com>
2021-05-21 15:32:06 -0400

    halfway through

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,14 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+
+    validates :age, numericality: {greater_than: 0}
+    validates :name, uniqueness: true  
+    validates :bio, length: {minimum: 30} 
+
+    def featured_post
+        post = self.posts.max_by {|post| post.likes}
+        post.title
+    end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  resources :bloggers, only: [:show, :new, :create]
+
+  resources :destinations, only: [:show]
+
+  resources :posts, only: [:show, :new, :create, :edit, :update]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
